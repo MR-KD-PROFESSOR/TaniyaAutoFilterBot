ece3effe2c9cf8699344f4693f2651da8f155f76
MR-KD-PROFESSOR <126370040+MR-KD-PROFESSOR@users.noreply.github.com>
2023-04-13 15:40:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,7 +24,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 # Bot images & videos
 PICS = (environ.get('PICS', 'https://telegra.ph/file/5553dc39f968b364d4856.jpg')).split()
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/d45b0e0b94ef267b94378.jpg")
-MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/bbabccf5c436689ccc9b9.mp4")
+MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/a6f43f759b8c4c977c913.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/2a888a370f479f4338f7c.jpg")
 
 # Admins, Channels & Users
